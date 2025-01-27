b04eec7eaa3bcabeba65a894f2f571e02db5a791
BrandonSeed <brandon@seedshouse.co.uk>
2025-01-27 14:57:07 +0000

    add tests for GET /api/topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,20 +1,13 @@
 const endpointsJson = require("../endpoints.json");
 const app = require('../app')
 const request = require('supertest')
-/* Set up your test imports here */
 
-/* Set up your beforeEach & afterAll functions here */
+const {articleData, commentData, topicData, userData} = require('../db/data/test-data/index')
+const db = require('../db/connection')
+const seed = require('../db/seeds/seed')
 
-describe("GET /api", () => {
-  test("200: Responds with an object detailing the documentation for each endpoint", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
-      });
-  });
-});
+beforeEach(() => seed({topicData, userData, articleData, commentData}))
+afterAll(() => db.end())
 
 describe('Non-endpoint request', () => {
   
@@ -28,3 +21,51 @@ describe('Non-endpoint request', () => {
     })
   });
 });
+
+describe("GET /api", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body: { endpoints } }) => {
+        expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe('GET /api/topics', () => {
+  
+  test('should respond with status 200 and an obeject with all the topics from the database', () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then(({ body: { topics } }) => {
+      expect(topics).toHaveLength(3)
+      topics.forEach((topic) => {
+        expect(topic).toHaveProperty('slug')
+        expect(topic).toHaveProperty('description')
+      })
+    })
+  });
+
+  test('should respond with the correct data stored in the database', () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then(({ body: { topics } }) => {
+      expect(topics).toEqual([{
+        description: 'The man, the Mitch, the legend',
+        slug: 'mitch'
+      },
+      {
+        description: 'Not dogs',
+        slug: 'cats'
+      },
+      {
+        description: 'what books are made of',
+        slug: 'paper'
+      }])
+    })
+  });
+});
+
