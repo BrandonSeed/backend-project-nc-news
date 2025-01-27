57d4f2700b66fb0bc059878203b56128a2c1a633
BrandonSeed <brandon@seedshouse.co.uk>
2025-01-27 14:28:10 +0000

    remove lingering conole.log

--- contollers/getApi.js
+++ contollers/getApi.js
@@ -2,7 +2,6 @@ const fetchApi = require('../models/fecthApi')
 
 function getApi(req, res) {
     fetchApi().then((result) => {
-        console.log(result)
         res.status(200).send({endpoints: result})
     })
 }
