93739b7e463ae2c4047e869abbe1eb10f52e50e7
BrandonSeed <brandon@seedshouse.co.uk>
2025-01-27 13:55:42 +0000

    add error handling for endpoints that don't exist, makes an error file to handle error middleware

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const getApi = require('./contollers/getApi')
+const { noEndpointError } = require('./serverErrors')
+
+app.use(express.json())
+
 
 app.get('/api', getApi)
 
 
+
+app.get('/*', (req ,res, next) => {
+    next({status: 404, msg: "That endpoint does not exist"})
+})
+
+app.use(noEndpointError)
 
 module.exports = app
